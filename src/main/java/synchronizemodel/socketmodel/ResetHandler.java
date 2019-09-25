146e18f33b79220505a91a9ffa4aea3d859ea484
xenon <lgasyou@163.com>
2019-09-25 16:45:13 +0800

    Refactored SocketServer; added Interface: IHandler.

---
+++ src/main/java/synchronizemodel/socketmodel/ResetHandler.java
@@ -0,0 +1,12 @@
+package synchronizemodel.socketmodel;
+
+import com.alibaba.fastjson.JSONObject;
+import util.TwoTuple;
+
+public class ResetHandler extends AbstractHandler {
+
+    public TwoTuple<JSONObject, Boolean> handle(JSONObject data) {
+        return null;
+    }
+
+}
